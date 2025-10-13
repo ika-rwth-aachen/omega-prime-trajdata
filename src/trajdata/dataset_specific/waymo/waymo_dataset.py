f1c081a9d07a4eed70fee3173e694d7de34f2395
Michael Schuldes <78906774+MichaelSchuldes@users.noreply.github.com>
2025-10-13 13:09:14 +0000

    add support for all subsets in waymo

--- src/trajdata/dataset_specific/waymo/waymo_dataset.py
+++ src/trajdata/dataset_specific/waymo/waymo_dataset.py
@@ -74,6 +74,8 @@ class WaymoDataset(RawDataset):
             dataset_name = "validation"
         elif self.name == "waymo_test":
             dataset_name = "testing"
+        elif self.name[6:] in WAYMO_DATASET_NAMES:
+            dataset_name = self.name[6:]
         self.dataset_obj = WaymoScenarios(
             dataset_name=dataset_name, source_dir=self.metadata.data_dir
         )
